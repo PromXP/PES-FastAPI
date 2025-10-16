ee2eb2f0ff1fcdb89b3714e0687d1992a01c7019
PromXP <adm.promxp@gmail.com>
2025-10-16 14:52:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from app import app
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host = '0.0.0.0', port = 10000, reload = True)
+    # uvicorn.run('main:app', host = '0.0.0.0', port = 10000, reload = True)
+    port = int(os.environ.get("PORT", 8000))  # Azure sets this automatically
+    uvicorn.run(app, host="0.0.0.0",port=port)
+
